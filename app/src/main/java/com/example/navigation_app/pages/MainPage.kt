6c177c45faa87e39f734cf1ce8f8f59673234def
prodip2404 <prodip2404@gmail.com>
2024-07-16 22:59:51 +0530

    commit me

---
+++ app/src/main/java/com/example/navigation_app/pages/MainPage.kt
@@ -0,0 +1,79 @@
+package com.example.navigation_app.pages
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigation_app.R
+import com.example.navigation_app.ui.theme.PurpleGrey80
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainPage(){
+    var nameField= remember {
+        mutableStateOf("")
+    }
+    var ageField= remember {
+        mutableStateOf("")
+    }
+    Scaffold(
+        topBar = {
+        TopAppBar(
+            title = { Text(text = "Main Page", fontSize = 20.sp) },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = colorResource(id = R.color.purple_500),
+                titleContentColor = Color.White
+            ),
+        )
+    },
+        content = { contentpadding->
+            Column(
+                Modifier
+                    .padding(contentpadding)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment. CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                OutlinedTextField(
+                    label ={ Text(text = "Name")},
+                    value = nameField.value,
+                    onValueChange ={nameField.value=it},
+                    colors = TextFieldDefaults.colors(
+                        focusedLabelColor = Color.Green,
+                        unfocusedIndicatorColor = Color.White,
+                    )
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                OutlinedTextField(label ={ Text(text = "Age")},
+                    value = ageField.value  , onValueChange = {ageField.value=it},
+                    colors = TextFieldDefaults.colors(
+                        focusedLabelColor = Color.Green,
+                        unfocusedIndicatorColor = Color.White,
+                    )
+
+
+                )
+            }
+        }
+    )
+
+}
